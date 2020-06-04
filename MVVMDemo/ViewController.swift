2a46f8d0f3603dfcbab7d52b9285cd0fa16eeba8
Steven <07599.steven.hsieh@ict888.net>
2020-06-04 16:59:58 +0800

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Steven Hsieh on 2020/6/4.
+//  Copyright © 2020 Steven Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVMDemoTests/MVVMDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVMDemoTests.swift
+//  MVVMDemoTests
+//
+//  Created by Steven Hsieh on 2020/6/4.
+//  Copyright © 2020 Steven Hsieh. All rights reserved.
+//
+
+import XCTest
+@testable import MVVMDemo
+
+class MVVMDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
